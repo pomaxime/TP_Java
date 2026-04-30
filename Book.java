32bbb73992786992bed2adc65e56fc8008d400be
aamaury <amaury.aune@ynov.com>
2026-04-30 08:46:20 +0200

    Le présent se fait vide et triste,

--- Book.java
+++ Book.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
 
 public abstract class Book {
     private String title;
-    private string author;
+    private String author;
     private int id;
+    private static List<Book> library = new ArrayList<>();
+
+    public Book(String title, String author, int id) {
+        this.title = title;
+        this.author = author;
+        this.id = id;
+        library.add(this);
+    }
 }

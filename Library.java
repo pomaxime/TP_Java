9e999aab1b27c5b1ee7f531b6d54ab0e8561c373
Benji <benjamin@lemoine.cloud>
2026-04-30 09:37:19 +0200

    feat[+]: Library.java => ajout de la fonction public Library, borrowBook et returnBook avec un systeme de if/else. Doit rajouter la partie try\catch demandé dans le pdf

--- Library.java
+++ Library.java
@@ -1,7 +1,51 @@
 public class Library {
     private Book book;
 
+    private static ArrayList<Book> library = new ArrayList<>();
+
     public showBook() {
         return Library;
     }
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+
+    public void borrowBook(int id) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getId() == id) {
+                found = true;
+                if (books.get(i).isBorrowed()) {
+                    System.out.println("Ce livre est déjà emprunté.");
+                } else {
+                    books.get(i).setBorrowed(true);
+                    System.out.println("Livre emprunté avec succès !");
+                }
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Aucun livre trouvé avec cet ID.");
+        }
+    }
+ 
+    public void returnBook(int id) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getId() == id) {
+                found = true;
+                if (!books.get(i).isBorrowed()) {
+                    System.out.println("Ce livre n'est pas emprunté.");
+                } else {
+                    books.get(i).setBorrowed(false);
+                    System.out.println("Livre retourné avec succès !");
+                }
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Aucun livre trouvé avec cet ID.");
+        }
+    }
+
 }

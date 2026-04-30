fe38822c7b285a90cdc0deef50889a653953e9e2
Benji <benjamin@lemoine.cloud>
2026-04-30 09:35:20 +0200

    feat[+]: Book.java => ajout de public getTitle, getAuthor, getId, isBorrowed et setBorrowed. Pour pouvoir les rappeler dans Library.java avec la fonction borrowBook et returnBook.

--- Book.java
+++ Book.java
@@ -4,12 +4,40 @@ public abstract class Book {
     private String title;
     private String author;
     private int id;
-    private static List<Book> library = new ArrayList<>();
+    private boolean borrowed;
 
     public Book(String title, String author, int id) {
         this.title = title;
         this.author = author;
         this.id = id;
-        library.add(this);
+        this.borrowed = false;
+        // library.add(this);
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // public static List<Book> getLibrary() {
+    //     return library;
+    // }
+
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        this.borrowed = borrowed;
+    }
+
+
+
 }
